f804c4e8fe822b031fcb1fa6f03c54e898cc3f7b
samniem <sami.nieminen@gmail.com>
2022-06-13 06:55:43 +0900

    Javascript in HTML

---
+++ querySelector/main.js
@@ -0,0 +1,6 @@
+const heading = document.querySelector('h1')
+heading.textContent = 'BIG GREETING!'
+const body = document.querySelector('body')
+body.style.backgroundColor = 'lavender'
+const p = document.querySelector('p')
+p.style.backgroundColor = 'white';
